cab694785e917534df39037a17174905ad841843
Tomas Alabes <tomas.alabes@gmail.com>
2015-08-22 20:32:33 -0700

    Server with parameter for css and js reqs

---
+++ app.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+//app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/index.html', function(req, res) {
+  res.sendfile('views/index.html')
+});
+
+app.get(['/stylesheets/*.css', '/javascripts/*.js'], function(req, res) {
+    var delay = req.query.delay ? req.query.delay * 1000 : 0;
+    setTimeout(function(){
+        console.log('req.path: '+ req.path);
+        res.sendfile('public/'+req.path);
+    }, delay);
+});
+
+module.exports = app;
